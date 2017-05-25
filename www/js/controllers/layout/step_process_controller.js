85ec59192fb2d738102dfdfbd346cf8443a208b6
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-25 13:36:27 +0700

    fixed lint

--- www/js/controllers/layout/step_process_controller.js
+++ www/js/controllers/layout/step_process_controller.js
@@ -10,12 +10,4 @@ function stepProcessCtr ($scope, $rootScope, UserService, DataService) {
     { step: '3', title: 'Health Questions', state: 'step2' },
     { step: '4', title: 'Health Questions', state: 'step2' },
     { step: '5', title: 'Document Upload', state: 'step2' }]
-
-  vm.changeStep = function (step) {
-    vm.policyStep = step || vm.policyStep
-    vm.pageList.forEach(function (page) {
-      if (page.step === step) defaultTab = page.defaultTab
-    })
-    this.changeTab(defaultTab)
-  }
 }
