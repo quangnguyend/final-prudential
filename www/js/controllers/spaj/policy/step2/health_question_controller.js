e6d15f48d8c2c7382761ac81f1052c489ef2e607
KhiemNguyenS3Corp <khiem.nguyen@s3corp.com.vn>
2017-05-25 18:16:56 +0700

    update health question

--- www/js/controllers/spaj/policy/step2/health_question_controller.js
+++ www/js/controllers/spaj/policy/step2/health_question_controller.js
@@ -27,11 +27,47 @@ function step2Ctrl ($state, $scope, $rootScope, $stateParams, UserService, DataS
       nodule_and_tumor: false
     },
     has_brain_related_illness: null,
-    has_hormone_or_autoimmune:false,
+    has_hormone_or_autoimmune: false,
     eye_condition: {
       is_using_contact_lens: false,
       has_eye_related_illness: false
     }
   }
 
+  $scope.showPopup = function () {
+    // custom popup
+    var eyePopup = $ionicPopup.show({
+      templateUrl: 'views/spaj/policy/step2/popup-eye.html',
+      title: 'Detail Kondisi Mata',
+      cssClass: 'popup-prudential',
+      scope: $scope,
+      buttons: [
+        {
+          text: '<i class="icon ion-ios-arrow-back"></i>',
+          type: 'btn-popup-back',
+          onTap: function (e) {
+            console.log('back clicked');
+            e.preventDefault();
+          }
+        },
+        {
+          text: '<i class="icon ion-android-close"></i>',
+          type: 'btn-popup-close',
+          onTap: function (e) {
+            eyePopup.close();
+          }
+        },
+        {
+          text: 'Selesai',
+          type: 'button-assertive btn-popup-save',
+          onTap: function (e) {
+            console.log('save clicked!');
+          }
+        }
+      ]
+    });
+    eyePopup.then(function (res) {
+      console.log('Tapped!', res);
+    });
+  }
 }
