6f67f6597d467d16a7567b4716e21e4e2fbbcbd9
Tin Bui <tin.bui@s3corp.com.vn>
2017-05-25 18:02:36 +0700

    update health_question

--- www/js/controllers/spaj/policy/step2/health_question_controller.js
+++ www/js/controllers/spaj/policy/step2/health_question_controller.js
@@ -4,4 +4,34 @@ function step2Ctrl ($state, $scope, $rootScope, $stateParams, UserService, DataS
   $rootScope.showMenu = true
 
   var vm = this
+
+  vm.healthData = {
+    weight: null,
+    height: null,
+    smoke_condition: {
+      is_smoking: false,
+      sticks_cigarettes_per_day: null
+    },
+    medicin_condition: {
+      is_using: false,
+      type_of_medicine: null
+    },
+    another_health_conditions: {
+      high_blood_pressure: false,
+      increased_cholesterol: false,
+      congenital_abnormalities: false,
+      abnormalities_of_heart_and_blood_vessels: false,
+      stroke: false,
+      rheuma: false,
+      chest_pain: false,
+      nodule_and_tumor: false
+    },
+    has_brain_related_illness: null,
+    has_hormone_or_autoimmune:false,
+    eye_condition: {
+      is_using_contact_lens: false,
+      has_eye_related_illness: false
+    }
+  }
+
 }
