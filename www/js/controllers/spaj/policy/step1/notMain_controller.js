1058ff198396387b722c8ef123b20af6cab8a9b4
Vuong Dao <vuong.dao@s3corp.com.vn>
2017-05-26 17:52:31 +0700

    Add tab and data binding for not main insured

--- www/js/controllers/spaj/policy/step1/notMain_controller.js
+++ www/js/controllers/spaj/policy/step1/notMain_controller.js
@@ -1,25 +1,39 @@
-function phNotMainCtrl ($scope, $rootScope, $stateParams, UserService, DataService) {
+function phNotMainCtrl($scope, $rootScope, SpajService) {
   $rootScope.showBar = true
   $rootScope.showBack = true
   $rootScope.showMenu = true
+  var MAIN_INSURED_TAB = 'MAIN_INSURED'
+  var rootSpajData = SpajService.getData('spaj')
+  console.log('rootSpajData', rootSpajData)
+  $scope.main_insured_tab = MAIN_INSURED_TAB
 
-  vm.Tambahan1 = true;
-  vm.Tambahan2 = false;
-  vm.tab = 1;
-  $scope.active = 1;
-  $scope.addTab = function(){
-  	if(vm.tab == 1){
-  		vm.Tambahan1 = true;	
-  		vm.tab += 1;
-  		$scope.active = 3;
-  	}
-  	else{
-  		vm.Tambahan2 = true;	
-  		vm.tab += 1;
-  		$scope.active = 4;
-  	}
+  $scope.currentTab = MAIN_INSURED_TAB
+  $scope.additionalList = []
+
+  $scope.switchTab = function (tab) {
+    $scope.currentTab = tab || MAIN_INSURED_TAB
   }
-  vm.clickTab = function(active){
-  	$scope.active = active;
+
+  $scope.addAdditionalTab = function () {
+    var numberTab = $scope.additionalList.length
+    // we have only maximum 2 addtional tabs
+    if (numberTab === 2) { return }
+    $scope.additionalList.push({
+      id: 'ADDITIONAL_' + numberTab,
+      name: 'Tertanggung Tambahan ' + (numberTab + 1)
+    })
+    $scope.currentTab = 'ADDITIONAL_' + numberTab
   }
+
+  $scope.nextClickHandle = function () {
+    console.dir(SpajService.getData())
+  }
+
+  function initPage(rootSpajData) {
+    if (!rootSpajData) return
+    if (rootSpajData.tambahan1) { $scope.addAdditionalTab() }
+    if (rootSpajData.tambahan2) { $scope.addAdditionalTab() }
+  }
+
+  initPage(rootSpajData)
 }
