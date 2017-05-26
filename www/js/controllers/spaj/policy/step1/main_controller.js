e8818e0ad32bac4ca1a824aa6a5f8d29caf7e682
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-26 15:32:22 +0700

    fixed error

--- www/js/controllers/spaj/policy/step1/main_controller.js
+++ www/js/controllers/spaj/policy/step1/main_controller.js
@@ -5,28 +5,27 @@ function phMainCtrl ($scope, $rootScope, $stateParams, UserService, DataService)
 
   var vm = this
 
-  vm.Tambahan1 = false;
-  vm.Tambahan2 = false;
-  vm.tab = 1;
-  $scope.active = 1;
-  $scope.addTab = function(){
-  	if(vm.tab == 1){
-  		vm.Tambahan1 = true;	
-  		vm.tab += 1;
-  		$scope.active = 3;
-  	}
-  	else{
-  		vm.Tambahan2 = true;	
-  		vm.tab += 1;
-  		$scope.active = 4;
-  	}
+  vm.Tambahan1 = false
+  vm.Tambahan2 = false
+  vm.tab = 1
+  $scope.active = 1
+  $scope.addTab = function () {
+    if (vm.tab == 1) {
+      vm.Tambahan1 = true
+      vm.tab += 1
+      $scope.active = 3
+    }   else {
+      vm.Tambahan2 = true
+      vm.tab += 1
+      $scope.active = 4
+    }
   }
 
-  vm.viewStep1 = function() {
-  	console.log(vm.tab);	
+  vm.viewStep1 = function () {
+    // console.log(vm.tab);
   }
 
-  vm.clickTab = function(active){
-  	$scope.active = active;
+  vm.clickTab = function (active) {
+    $scope.active = active
   }
 }
--- www/js/controllers/spaj/policy/step1/notMain_controller.js
+++ www/js/controllers/spaj/policy/step1/notMain_controller.js
@@ -2,24 +2,23 @@ function phNotMainCtrl ($scope, $rootScope, $stateParams, UserService, DataServi
   $rootScope.showBar = true
   $rootScope.showBack = true
   $rootScope.showMenu = true
-  vm = this
-  vm.Tambahan1 = true;
-  vm.Tambahan2 = false;
-  vm.tab = 1;
-  $scope.active = 1;
-  $scope.addTab = function(){
-  	if(vm.tab == 1){
-  		vm.Tambahan1 = true;	
-  		vm.tab += 1;
-  		$scope.active = 3;
-  	}
-  	else{
-  		vm.Tambahan2 = true;	
-  		vm.tab += 1;
-  		$scope.active = 4;
-  	}
+  var vm = this
+  vm.Tambahan1 = true
+  vm.Tambahan2 = false
+  vm.tab = 1
+  $scope.active = 1
+  $scope.addTab = function () {
+    if (vm.tab == 1) {
+      vm.Tambahan1 = true
+      vm.tab += 1
+      $scope.active = 3
+    }   else {
+      vm.Tambahan2 = true
+      vm.tab += 1
+      $scope.active = 4
+    }
   }
-  vm.clickTab = function(active){
-  	$scope.active = active;
+  vm.clickTab = function (active) {
+    $scope.active = active
   }
 }
