d6221c3de08985ed1a96775901ad232aa4077fcf
Vuong Dao <vuong.dao@s3corp.com.vn>
2017-05-26 13:58:35 +0700

    Add tab-bar component

--- www/js/component.js
+++ www/js/component.js
@@ -14,3 +14,24 @@ angular.module('prudential.components', [])
       // console.log(ctrl)
     }
   })
+  .component('tabBar', {
+    transclude: true,
+    template: `<div class='row tab-menu'>
+      <span ng-repeat='(index, item) in $ctrl.list' ng-click='$ctrl.clickHandle(index)' 
+       ng-class='$ctrl.currentTab===index?"active":""' class='col'>{{item.title}}</span>
+    </div>`,
+    controller: function ($attrs) {
+      var ctrl = this
+      var list = [
+        { title: 'Menu 1', onClick: function () { alert(1) } },
+        { title: 'Menu 2', onClick: function () { alert(2) } },
+        { title: 'Menu 3', onClick: function () { alert(3) } }
+      ]
+      ctrl.currentTab = 0
+      ctrl.list = $attrs.list || list
+      ctrl.clickHandle = function (index) {
+        ctrl.currentTab = index
+        ctrl.list[ctrl.currentTab].onClick()
+      }
+    }
+  })
