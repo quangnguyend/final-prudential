afcf0aba07951db4532006c96c7f9c919e23ea1d
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-26 14:45:17 +0700

    edit code of Minh

--- www/js/controllers/spaj/policy/step1/step1_controller.js
+++ www/js/controllers/spaj/policy/step1/step1_controller.js
@@ -10,11 +10,12 @@ function step1Ctrl ($state, $scope, $rootScope, $stateParams, SpajService) {
   // console.log(SpajService.getData('spaj'))
 
   function gotoPhPage () {
-    if (vm.objSpaj.utama && vm.objSpaj.typeSpaj=='PemegangPolis') {
-      $state.go('app.main')
-    }
-    else {
-       $state.go('app.notmain')
-    }
+    $state.go('app.main')
+    // if (vm.objSpaj.utama && vm.objSpaj.typeSpaj=='PemegangPolis') {
+    //   $state.go('app.main')
+    // }
+    // else {
+    //    $state.go('app.notmain')
+    // }
   }
 }
