aeb1d58906a68b2fd22d8c8d69d002999509a8be
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-29 13:58:46 +0700

    fixed gulp lint

--- www/js/controllers/spaj/policy/step4/payor_option1_controller.js
+++ www/js/controllers/spaj/policy/step4/payor_option1_controller.js
@@ -1,15 +1,14 @@
 function payorOption1Ctrl ($scope, DataService, $ionicScrollDelegate, $location) {
-
-  var vm = this;
+  var vm = this
   vm.tabs = [
     { title: 'Tertanggung Utama', value: 'payor_option0' },
-    { title: 'Tertanggung Tambahan 1', value: 'payor_option1' },
-  ];
-  vm.currentTab = 'payor_option0';
+    { title: 'Tertanggung Tambahan 1', value: 'payor_option1' }
+  ]
+  vm.currentTab = 'payor_option0'
 
   vm.save = function () {
     // var data = vm.questions;
     // console.log(data);
     $location.path('/app/step4/option1')
-  };
+  }
 }
