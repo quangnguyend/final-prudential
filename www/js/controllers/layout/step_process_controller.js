4dbc4cb0fd87ca9ce9b5ce34defc2d7fcde23fa3
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-29 13:48:02 +0700

    add step

--- www/js/controllers/layout/step_process_controller.js
+++ www/js/controllers/layout/step_process_controller.js
@@ -3,22 +3,23 @@ function stepProcessCtr ($scope, $rootScope, UserService, DataService, $location
   $rootScope.showBack = true
   $rootScope.showMenu = true
   var vm = this
-  var step = $location.path().split("/")[2].replace(/step/i, '');
-  if( step === undefined || step === null || step === 'main' )
-    $rootScope.currentStep = '1';
-  else{
-    $rootScope.currentStep = step;
+  var step = $location.path().split('/')[2].replace(/step/i, '')
+  if (step === undefined || step === null || step === 'main') { $rootScope.currentStep = '1' } else {
+    $rootScope.currentStep = step
   }
-  vm.policyStep = $rootScope.currentStep;
+  vm.policyStep = $rootScope.currentStep
 
   vm.pageList = [
     { step: '1', title: 'Policy Holder Information', state: 'app.main'},
     { step: '2', title: 'Health Questions', state: 'step2' },
-    { step: '3', title: 'Health Questions', state: 'step3' },
-    { step: '4', title: 'Payor', state: 'step4' },
-    { step: '5', title: 'Beneficiaries', state: 'step5' }]
+    { step: '3', title: 'Kegiatan beresiko', state: 'step3' },
+    { step: '4', title: 'Pembayar Premi', state: 'step4' },
+    { step: '5', title: 'Penerima Manfaat', state: 'step5' },
+    { step: '6', title: 'Health Questions', state: 'step6' },
+    { step: '7', title: 'Health Questions', state: 'step7' },
+    { step: '8', title: 'Payor', state: 'step8' }]
 
   vm.changeStep = function (step) {
-    vm.policyStep = step;
+    vm.policyStep = step
   }
 }
