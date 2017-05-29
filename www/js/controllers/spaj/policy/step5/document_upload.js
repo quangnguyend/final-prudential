da512d459c08a0b997a38ca6e608ad8f07df54d6
Chinh Ngo <chinh.ngo@s3corp.com.vn>
2017-05-29 11:42:43 +0700

    integrate function step 3 and 5

--- www/js/controllers/spaj/policy/step5/document_upload.js
+++ www/js/controllers/spaj/policy/step5/document_upload.js
@@ -1,4 +1,4 @@
-function documentUploadCtrl ($scope, $rootScope, Upload) {
+function documentUploadCtrl ($scope, $rootScope, Upload, SpajService) {
   $rootScope.showBar = true
   $rootScope.showBack = true
   $rootScope.showMenu = true
@@ -8,7 +8,22 @@ function documentUploadCtrl ($scope, $rootScope, Upload) {
     {title: 'Pemegang Polis', value: 'policy_holders'},
     {title: 'Tertanggung Tambahan 1', value: 'additional_insured'}
   ];
-  vm.currentTab = 'premiums';
+
+  var condition = SpajService.getData('spaj');
+  console.log(condition);
+  if (typeof condition != 'undefined'){
+    if(condition.utama == true && condition.typeSpaj == 'PemegangPolis'){
+      vm.currentTab = 'premiums';
+    }else if(condition.typeSpaj == 'PemegangPolis'){
+      vm.currentTab = 'policy_holders';
+    }else if(condition.typeSpaj == '' && condition.tambahan1 == true){
+      vm.currentTab = 'additional_insured';
+    }else{
+      vm.currentTab = 'premiums';
+    }
+  }else{
+    vm.currentTab = 'premiums';
+  }
 
   vm.switchTab = function (tab) {
     vm.currentTab = tab;
