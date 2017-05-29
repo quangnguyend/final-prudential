1b8ce866acb857c1195264eda2b6ff82997ef2d7
Tin Bui <tin.bui@s3corp.com.vn>
2017-05-29 17:22:46 +0700

    update reminder step

--- www/js/controllers/spaj/policy/reminder_step/reminder_controller.js
+++ www/js/controllers/spaj/policy/reminder_step/reminder_controller.js
@@ -11,50 +11,6 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
     null_properties: []
   }
 
-  var tempDataStructure = {
-    "personalAccidentPopupData": {
-      "function_name": "Bila kecelakaan menyangkut alat gerak, bagaimana fungsi alat gerak tersebut saat ini?",
-      "is_pen_installed": true
-    },
-    "eyePopupData": {
-      "name_of_illness": "Nama Keadaan/Penyakit",
-      "when_the_condition_found": "Kapan kondisi ditemukan",
-      "last_care_date": "Tanggal Konsultati/Perawatan terakhir",
-      "hospoital_name": "Nama Rumah sakit yang dikunjungi",
-      "hospital_address": "Alamat Rumah sakit yang dikunjungi",
-      "medicine": "Tindakan Medis"
-    },
-    "height": null,
-    "weight": null,
-    "smoker": 123,
-    "sticks_cigarettes": 10,
-    "medication": 321,
-    "medication_detail": "5",
-    "darah": 123,
-    "ipeningkatan": null,
-    "ijantung": null,
-    "ikelainan": null,
-    "istroke": null,
-    "idemam": false,
-    "inyeri": true,
-    "inodule": false,
-    "ibrain": true,
-    "ihocmon": false,
-    "ieye": true,
-    "eye_contact_lenses": true,
-    "eye_disorders": true,
-    "iear": true, "irespiratory": true,
-    "iheart": true, "idigestive": true,
-    "ikidney": true, "iface": true,
-    "ibone": true, "itumors": true,
-    "iill": true, "iinjury": true,
-    "iinjury_option_1": true,
-    "idisease": true, "idisorder": true,
-    "iabnormality": true, "ipapsmear": true,
-    "ipregnant": true, "isurgery": true,
-    "icomplication": true, "ilostweight": true
-  }
-
   vm.data = {
     policy: [{
       insurance_type: "",
@@ -62,15 +18,24 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
       sum_insured: "",
       is_substandard: false
     }],
+    healthData: {},
     null_properties: []
   }
 
   var validateHealthData = function (obj, returnObj) {
     if (obj) {
       for (let prop in obj) {
-        // console.log(typeof obj[prop], typeof obj[prop] == 'object')
-        // console.log(prop)
-        if (typeof obj[prop] === 'object' && obj[prop]) {
+        if (prop === 'personalAccidentPopupData') {
+          if (obj['ieye'] === true) {
+            validateHealthData(obj[prop], returnObj)
+          }
+        }
+        if (prop === 'eyePopupData') {
+          if (obj['iinjury'] === true) {
+            validateHealthData(obj[prop], returnObj)
+          }
+        }
+        else if (prop != 'personalAccidentPopupData' && prop != 'eyePopupData' && typeof obj[prop] === 'object' && obj[prop]) {
           validateHealthData(obj[prop], returnObj)
         }
         else if (obj[prop] === null || obj[prop] === "") {
@@ -81,8 +46,7 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
     }
   }
 
-  validateHealthData(tempDataStructure, vm.healthFormValidStatus)
-  console.log(vm.healthFormValidStatus)
+  validateHealthData(vm.healthData, vm.healthFormValidStatus)
 
   vm.const = {
     type_of_insurances: [
@@ -138,7 +102,7 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
       inodule: "Nodule/Tumor/ Kista/ Pembengkakan/Pertumbuhan/pembesaran lainnya (kanker/non-kanker)",
       ibrain: "Kondisi Terkait Otak, Saraf, Saraf Tulang Belakang",
       ihocmon: "Kondisi Hormon atau Autoimun",
-      //Todo
+      //Todo (move to onen object in which contain both ieye and eyePopupData)
       ieye: "Kondisi mata",
       eye_contact_lenses: "Rabun jauh dengan menggunakan kacamata softlens",
       eye_disorders: "Katarak atau kelainan mata lainnya",
@@ -151,7 +115,7 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
       ibone: "Kondisi Tulang, Sendi & Otot",
       itumors: "Tumor & Pertumbuhan",
       iill: "Penyakit Infeksi, Penyakit Menular Seksual & AIDS",
-      //Todo
+      //Todo (move to onen object in which contain both iinjury and personalAccidentPopupData)
       iinjury: "Kecelakaan Pribadi / Cedera jangka panjang",
       idisease: "Penyakit lain yang belum disebutkan?",
       idisorder: "Apakah Calon Tertanggung mempunyai keluhan/kelainan/gangguan pada Payudara atau Prostat?",
@@ -163,8 +127,6 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
       ilostweight: "Berat Badan waktu Lahir kurang dari atau sama dengan 2500 gram?"
     }
   }
-
-
 
   vm.addPolicy = function () {
     let newPolicy = {
@@ -183,7 +145,8 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
   vm.submit = function () {
     if (vm.healthFormValidStatus.null_properties > 5) { return }
     else {
-      SpajService.setData('reminder_step', vm)
+      vm.data.healthData = vm.healthData;
+      SpajService.setData('reminder_step', vm.data)
     }
   }
   vm.review = function () {
@@ -228,7 +191,6 @@ function reminderCtrl ($scope, $rootScope, $state, SpajService) {
   //   "ikidney": true, "iface": true,
   //   "ibone": true, "itumors": true,
   //   "iill": true, "iinjury": true,
-  //   "iinjury_option_1": true,
   //   "idisease": true, "idisorder": true,
   //   "iabnormality": true, "ipapsmear": true,
   //   "ipregnant": true, "isurgery": true,
