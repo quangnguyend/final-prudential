5810c3bd598d57fbe7b690bc4cf2c8ef58c3c12b
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-06-01 16:21:46 +0700

    add title header and process

--- www/js/controllers/layout/custom_header_controller.js
+++ www/js/controllers/layout/custom_header_controller.js
@@ -1,6 +1,22 @@
-function customHeaderCtrl ($scope, $rootScope, $attrs, $timeout) {
+function customHeaderCtrl ($state, $scope, $rootScope, $attrs, $timeout) {
   var vm = this
   vm.title = $attrs.title
-  vm.step = $attrs.step
-  $rootScope.setCurrentPolicyStep($attrs.step)
+  var currentStep = $attrs.step
+
+  vm.steps = $rootScope.getPolicySteps()
+  if (!$rootScope.policyStep) return
+  vm.steps = $rootScope.getPolicySteps()
+  vm.currentStep = $rootScope.getCurrentPolicyStep()
+  vm.selectStep = vm.currentStep
+
+  vm.selectStep = currentStep
+  vm.changeStep = function () {
+    for (var i in vm.steps) {
+      if (vm.steps[i].step === vm.selectStep) {
+        $state.go('app.' + vm.steps[i].state)
+        $rootScope.setCurrentPolicyStep(vm.selectStep)
+        return
+      }
+    }
+  }
 }
