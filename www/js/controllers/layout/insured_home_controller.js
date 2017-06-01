7c67c9a8e01ce71f30185ac75bc63d452d75a0f9
Vuong Dao <vuong.dao@s3corp.com.vn>
2017-06-01 14:03:22 +0700

    Fix standard syntax

--- www/js/controllers/layout/insured_home_controller.js
+++ www/js/controllers/layout/insured_home_controller.js
@@ -2,33 +2,33 @@ function insuredHomeCtrl ($scope, $rootScope, $attrs, $timeout, SpajService) {
   $rootScope.showBar = true
   $rootScope.showBack = true
   $rootScope.showMenu = true
-  var vm=this;
+  var vm = this
 
-  vm.insuredData={
-    address:[{}]
+  vm.insuredData = {
+    address: [{}]
   }
-  vm.addInsuredAddress=function(){
+  vm.addInsuredAddress = function () {
     vm.insuredData.address.push({})
   }
   $scope.data = {}
   var data = {}
 
   $scope.conditionSelected = false
-  $scope.$watch('[conditionSelected]', function(){
-    $scope.nextButtonDisabled = !$scope.conditionSelected;
-  }, true );
+  $scope.$watch('[conditionSelected]', function () {
+    $scope.nextButtonDisabled = !$scope.conditionSelected
+  }, true)
 
-  $scope.getValueButton= function(value){
-    $scope.option = value;
+  $scope.getValueButton = function (value) {
+    $scope.option = value
   }
 
-  $scope.$watch('[option]', function(){
+  $scope.$watch('[option]', function () {
     $rootScope.dataSave = {
-    conditionSelected: $scope.conditionSelected,
-    valueButtonSubmit: $scope.option
-  }
-  }, true );
-  //console.log( SpajService.getData('spaj1'));
+      conditionSelected: $scope.conditionSelected,
+      valueButtonSubmit: $scope.option
+    }
+  }, true)
+  // console.log( SpajService.getData('spaj1'));
 
   $timeout(function () {
     var insuredName = $attrs.insuredData
@@ -64,5 +64,4 @@ function insuredHomeCtrl ($scope, $rootScope, $attrs, $timeout, SpajService) {
     if (data.name) { return true }
     return false
   }
-
 }
