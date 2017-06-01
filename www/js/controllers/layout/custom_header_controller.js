bfd9a66a3e5510d25f39f0177a2e09ef331633de
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-06-01 16:36:53 +0700

    added index

--- www/js/controllers/layout/custom_header_controller.js
+++ www/js/controllers/layout/custom_header_controller.js
@@ -3,12 +3,8 @@ function customHeaderCtrl ($state, $scope, $rootScope, $attrs, $timeout) {
   vm.title = $attrs.title
   var currentStep = $attrs.step
 
-  vm.steps = $rootScope.getPolicySteps()
   if (!$rootScope.policyStep) return
   vm.steps = $rootScope.getPolicySteps()
-  vm.currentStep = $rootScope.getCurrentPolicyStep()
-  vm.selectStep = vm.currentStep
-
   vm.selectStep = currentStep
   vm.changeStep = function () {
     for (var i in vm.steps) {
